5e7625f1870bb512a3a657966efdd4a95384e03a
Michiel Barnaart <michielbarnaart@gmail.com>
2018-11-26 22:21:49 +0100

    added score component(WIP)

--- learnbyplaying-client/src/components/score/Score.js
+++ learnbyplaying-client/src/components/score/Score.js
@@ -11,18 +11,65 @@ const styles = theme =>
       left: 0,
       right: 0,
       position: 'absolute',
-      top: '180px',
+      top: '120px',
       margin: '0 auto'
+    },
+    addPoints: {
+      color: 'green',
+      transform: 'translate(0, 50px)'
+    },
+    deductPoints: {
+      color: 'red',
+      transform: 'translate(0, -50px)'
     }
   });
 
 class Score extends Component {
+  points;
+  state = {
+    in: false
+  };
+  componentDidUpdate(prevProps) {
+    if (prevProps.score !== this.props.score) {
+      this.points = this.props.score - prevProps.score;
+      this.updateFade();
+    }
+  }
+  updateFade = () => {
+    this.setState({
+      in: !this.state.in
+    });
+  };
   render() {
+    // const { in } = this.state;
     const { score, classes } = this.props;
-    return <div className={classes.score}>Score: {score}</div>;
+    return (
+      <div className={classes.score}>
+        <h2>SCORE</h2>
+        <h3>{score}</h3>
+        {this.points && (
+          <Fade
+            in={this.state.in}
+            timeout={{ enter: 2000, exit: 5000 }}
+            onEntered={this.updateFade}
+          >
+            <p
+              className={
+                this.points > 0 ? classes.addPoints : classes.deductPoints
+              }
+            >
+              {this.points}
+            </p>
+          </Fade>
+        )}
+      </div>
+    );
   }
 }
 
-Score.propTypes = {};
+Score.propTypes = {
+  score: PropTypes.number,
+  classes: PropTypes.object
+};
 
 export default withStyles(styles)(Score);
